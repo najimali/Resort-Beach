3f3cc37c253fbf2b6b2441f9c7d6d3e6602522bd
najimali <najim.ali32@gmail.com>
2020-10-04 10:28:22 +0530

    Banner Added

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-export default function Hero({ chilren, hero }) {
-  return <header className={hero}>{chilren}</header>;
+export default function Hero({ children, hero }) {
+  return <header className={hero}>{children}</header>;
 }
 
 // When we forget to pass hero page para then this defaultHero css class will be used
--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,7 +1,11 @@
 import React, { Component } from "react";
-
-export default class SingleRoom extends Component {
+import { withRouter } from "react-router";
+class SingleRoom extends Component {
+  state = {
+    id: this.props.match.params.slug,
+  };
   render() {
-    return <div>Hello from Single Room Page</div>;
+    return <div>Hello from Single Room Page & id is {this.state.id}</div>;
   }
 }
+export default withRouter(SingleRoom);
