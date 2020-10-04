86f5402347bed1be72bef0cb990497a0326594b9
najimali <najim.ali32@gmail.com>
2020-10-04 10:36:28 +0530

    Error Page Done

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,5 +1,15 @@
 import React from "react";
 import Hero from "../components/Hero";
+import { Link } from "react-router-dom";
+import Banner from "../components/Banner";
 export default function Error() {
-  return <Hero></Hero>;
+  return (
+    <Hero>
+      <Banner title="404" subtitle="Page not found">
+        <Link to="/" className="btn-primary">
+          return Home{" "}
+        </Link>
+      </Banner>
+    </Hero>
+  );
 }
