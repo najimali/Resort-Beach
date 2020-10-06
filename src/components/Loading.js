00d61e295a23866fea71b3dd389491092ff21e3b
najimali <najim.ali32@gmail.com>
2020-10-06 21:28:58 +0530

    Contentful Setup

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -3,7 +3,7 @@ import loadingGif from "../images/gif/loading-arrow.gif";
 export default function Loading() {
   return (
     <div className="loading">
-      <h4>Room Data Laoding...</h4>
+      <h4>Room Data Loading...</h4>
       <img src={loadingGif} alt="" />
     </div>
   );
