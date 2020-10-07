cc67112d2d7a9c4cc42adf2c14ff7c15e477593c
najimali <najim.ali32@gmail.com>
2020-10-07 12:56:31 +0530

    Hosting the project

--- src/context.js
+++ src/context.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import items from "./data";
+// import items from "./data";
 import Client from "./Contentful";
 const RoomContext = React.createContext();
 class RoomProvider extends Component {
@@ -48,7 +48,7 @@ class RoomProvider extends Component {
     }
   };
   componentDidMount() {
-    //this.getData();
+    this.getData();
     // let rooms = this.formatData(items);
     // let featuredRooms = rooms.filter((room) => room.featured === true);
     // let maxPrice = Math.max(...rooms.map((item) => item.price));
